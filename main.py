c55f8341315c02e88c60bc2d4108a5560476c638
Bobby Alestra <bobbyalestra135@gmail.com>
2021-11-05 17:02:48 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import pygame
+from pygame.locals import *
+
+pygame.init()
+
+
+screen_width = 1000
+screen_height = 1000
+
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption("Bobby's Platform Game")
